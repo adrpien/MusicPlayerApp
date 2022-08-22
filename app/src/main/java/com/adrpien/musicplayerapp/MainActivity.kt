fb6d92791906a92321d5a9573e6d77ed6348d92e
Adrian Pienkowski <adrpien@gmail.com>
2022-08-22 16:16:10 +0200

    - creating fragments for navHost - bottom navigation menu implementation using NavigationUI

---
+++ app/src/main/java/com/adrpien/musicplayerapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.adrpien.musicplayerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.adrpien.musicplayerapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Setting bottomNaviationMenu
+        binding.bottomNavigationMenu.setupWithNavController(findNavController(R.id.fragmentContainerView))
+    }
+
+
+}
